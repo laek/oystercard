d7861b3a4fd7d2d0b4a0b1b7adcf70166bd26e90
Laura Kytonen <laura.kytonen@gmail.com>
2019-12-18 14:45:30 +0000

    basic oystercard functionality and tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,16 +2,39 @@ class Oystercard
 
   attr_reader :balance, :max_balance
 
-  MAX_BALANCE = 90.00
+  MAX_BALANCE = 90
+  BALANCE_TO_TRAVEL = 1
+  MINIMUM_CHARGE = 1
 
   def initialize
     @balance = 0.00
     @max_balance = MAX_BALANCE
+    @currently_in_journey = false
   end
 
   def top_up(amount)
     raise "Top up not possible, maximum balance of #{@max_balance} exceeded." if (@balance + amount) > @max_balance
     @balance += amount
   end
+
+  def touch_in
+    raise "Please top up, you have insuffient balance" if @balance < BALANCE_TO_TRAVEL
+    @currently_in_journey = true
+  end
+
+  def touch_out
+    deduct(MINIMUM_CHARGE)
+    @currently_in_journey = false
+  end
+
+  def in_journey?
+    @currently_in_journey
+  end
+
+  private
+  
+    def deduct(amount)
+      @balance -= amount
+    end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,39 +2,76 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it "initialises a new instance of the class Oystercard" do
-    expect(subject).to be_a Oystercard
+  context "basic set up" do
+    it "initialises a new instance of the class Oystercard" do
+      expect(subject).to be_a Oystercard
+    end
+
+    it "has a balance of 0.00 initially" do
+      expect(subject.balance).to eq 0
+    end
+
+    it "is initially not in a journey" do
+      expect(subject).not_to be_in_journey
+    end
   end
 
-  it "has a balance of 0.00 initially" do
-    expect(subject.balance).to eq 0
+  it "allows the user to top up money to the card" do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
   end
 
-  describe "#top_up" do
-    context "allows user to top up" do
-      it "responds to a method called top up" do
-        expect(subject).to respond_to(:top_up)
-      end
+  context "methods and constants" do
+    it "responds to a method called top up" do
+      expect(subject).to respond_to(:top_up)
+    end
 
-      it "allows the user to top up money to the card" do
-        subject.top_up(10)
-        expect(subject.balance).to eq 10
-      end
+    it "responds to a method called touch_in" do
+      expect(subject).to respond_to(:touch_in)
+    end
 
-      it "has a maximum balance of £90" do
-        expect(subject.max_balance).to eq 90.00
-      end
+    it "responds to a method called touch out" do
+      expect(subject).to respond_to(:touch_out)
+    end
+
+    it "responds to a method called in_journey?" do
+      expect(subject).to respond_to(:in_journey?)
+    end
+
+    it "has a maximum balance of £90" do
+      expect(subject.max_balance).to eq 90.00
+    end
+
+    #it "has a minimum balance of £1 for touching in" do
+  end
+
+  context "when card has been topped up" do
+   before {subject.top_up(Oystercard::MAX_BALANCE)}
 
       it "raises an error when a user tries to top up the balance above £90" do
-        subject.top_up(90)
         expect {subject.top_up(1) }. to raise_error "Top up not possible, maximum balance of #{Oystercard::MAX_BALANCE} exceeded."
+      end
+
+      it "allows the user to touch in" do
+        subject.touch_in
+        expect(subject.in_journey?).to eq true
+      end
+
+      it "can touch out" do
+        subject.touch_in
+        subject.touch_out
+        expect(subject).not_to be_in_journey
+      end
+
+      it "charges fare when touching out" do
+        subject.touch_in
+        expect{ subject.touch_out}.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
       end
     end
 
-    describe "#deduct" do
-      context "allows user to use balance to pay for trips" do
-        
-      end
+  context "when card has no balance" do
+    it "raises an error when balance is below minimum balance" do
+      expect{subject.touch_in}.to raise_error ("Please top up, you have insuffient balance")
     end
   end
 end
